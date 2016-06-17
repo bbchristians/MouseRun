00614ec9b728479ed599841e425c44786c5f6a03
bbc7909 <bbc7909@rit.edu>
2016-06-17 14:24:31 -0400

    Added functionality for random generation of levels.

--- MovementTest/Assets/Scripts/Init/GameManager.cs
+++ MovementTest/Assets/Scripts/Init/GameManager.cs
@@ -9,13 +9,14 @@ public class GameManager : MonoBehaviour {
     public bool debug; // Determines if the game will run in debug mode
     public string debugFilePath; // The file Path leading to the debug file
     public int boardDim; // The dimensions of the board (must be square)
+    public int numObstacles; // The number of obstacles to randomly generate if not debugging
 
     //Prefab lists
     public GameObject[] basicObstaclePrefabs; // The BasicObstacle prefabs
     public GameObject[] buttonDoorPrefabs; // The button and door obstacle prefabs
     public GameObject[] wallPrefabs; // The wall prefabs
 
-    private ArrayList initList; // The List where information generated while loading from a file will be generated
+    private Queue preInitQueue; // The queue where information generated while loading from a file or through randomization will be generated
     private Queue initQueue; // The queue where the InitObstacles will be store for initialization
 
     // Generates the initList from a file given by fileName
@@ -28,26 +29,62 @@ public class GameManager : MonoBehaviour {
         while( (line = reader.ReadLine()) != null)
         {
             parts = line.Split();
-            initList.Add(new InitObstacle(parts[0].ToCharArray()[0], 
+            preInitQueue.Enqueue(new InitObstacle(parts[0].ToCharArray()[0], 
                 Int32.Parse(parts[1]), Int32.Parse(parts[2])));
         }
     }
 
+    // Generates the initList as random objects
+    private void RandomList()
+    {
+        for( int i = 0; i < numObstacles; i++)
+        {
+            int randRow = Random.Range(0, boardDim);
+            int randCol = Random.Range(0, boardDim);
+            preInitQueue.Enqueue(new InitObstacle('b', randRow, randCol));
+        }
+
+        // Here we will test to see if the level is solvable, and we will recursively call to redo if it isn't
+    }
+
     // Uses the InitObstacles from the initList to generate a queue of InitObjects to be 
-    // Initialized later
+    // Initialized later. After this step, all Queued InitObjects should be valid and ready to place.
     private void QueueInitObstacles()
     {
-        foreach( InitObstacle iob in initList)
+        ArrayList occupiedCoordinates = new ArrayList(); // Contains Vector2s representing the occupied spaces in the grid
+
+        occupiedCoordinates.Add(new Vector2(0,0)); // Occupy player spawn
+        occupiedCoordinates.Add(new Vector2(boardDim - 1, boardDim - 1)); // Occupy goal
+
+        InitObstacle iob;
+
+        while( preInitQueue.Count > 0 )
         {
-            if( !iob.OutOfBounds(boardDim))
+            iob = (InitObstacle)preInitQueue.Dequeue();
+            Vector2 iobCoords = new Vector2(iob.getRow(), iob.getCol());
+
+            if( !iob.OutOfBounds(boardDim) && !occupiedCoordinates.Contains(iobCoords))
             {
                 initQueue.Enqueue(iob);
-            } else if ( debug )
+                occupiedCoordinates.Add(iobCoords); // Occupy this object's coordinates
+            } else if ( !debug )
+            {
+                // Add a new initObstacle of the same type to the initList
+                int randRow = Random.Range(0, boardDim);
+                int randCol = Random.Range(0, boardDim);
+                initQueue.Enqueue(new InitObstacle(iob.GetCode(), randRow, randCol));
+            } else
             {
                 Debug.Log(iob + " Not added to initQueue.");
             }
         }
     } 
+
+    // Not Implemented
+    private bool IsntOccupied(InitObstacle iob, ArrayList occupiedCoordinates)
+    {
+        return false;
+    }
 
     // Generates the grid by populating it with GameObjects
     private void GenerateGrid()
@@ -91,7 +128,7 @@ public class GameManager : MonoBehaviour {
     }
 
     // Generates the outer wall of the board
-    void GenerateWall()
+    private void GenerateWall()
     {
         for( int i = -1; i <= boardDim; i++)
         {
@@ -105,14 +142,19 @@ public class GameManager : MonoBehaviour {
     }
 
 	void Start () {
-        initList = new ArrayList();
+        preInitQueue = new Queue();
         initQueue = new Queue();
 
-        // Generate initList
-	    if( debug )
+        // Generate initList from file
+        if (debug)
         {
             ReadFile(debugFilePath);
-        } // Else randomly generate
+        }
+        // Generate initlist randomly
+        else
+        {
+            RandomList();
+        }
 
         // Generate initQueue
         QueueInitObstacles();
