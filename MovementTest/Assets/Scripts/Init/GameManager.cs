b8c0e9b3d40becdfd480be5be8c6f2afeb039f63
bbc7909 <bbc7909@rit.edu>
2016-06-17 13:49:53 -0400

    Fixed out of bounds problems with the player.

--- MovementTest/Assets/Scripts/Init/GameManager.cs
+++ MovementTest/Assets/Scripts/Init/GameManager.cs
@@ -11,8 +11,9 @@ public class GameManager : MonoBehaviour {
     public int boardDim; // The dimensions of the board (must be square)
 
     //Prefab lists
-    public GameObject[] basicObstaclePrefabs; // The BasicObstacle prefab list
+    public GameObject[] basicObstaclePrefabs; // The BasicObstacle prefabs
     public GameObject[] buttonDoorPrefabs; // The button and door obstacle prefabs
+    public GameObject[] wallPrefabs; // The wall prefabs
 
     private ArrayList initList; // The List where information generated while loading from a file will be generated
     private Queue initQueue; // The queue where the InitObstacles will be store for initialization
@@ -51,12 +52,14 @@ public class GameManager : MonoBehaviour {
     // Generates the grid by populating it with GameObjects
     private void GenerateGrid()
     {
-        foreach( InitObstacle iob in initQueue)
+        GameObject instantiateGO; 
+        foreach ( InitObstacle iob in initQueue)
         {
-            GameObject instantiateGO = null;
+            instantiateGO = null;
+            // Store game object to be generated based off the GameObjectCode of iob
             switch( iob.GetCode())
             {
-                case 'b': instantiateGO = basicObstaclePrefabs[Random.Range(0, basicObstaclePrefabs.Length - 1)];
+                case 'b': instantiateGO = RandomFromArray(basicObstaclePrefabs);
                     break;
             }
             if( instantiateGO == null) // Dont instantiate a null GameObject
@@ -67,8 +70,37 @@ public class GameManager : MonoBehaviour {
                 }
                 continue;
             }
-            // Vector dimensions : (girdPos * scale) + (.5 * modelSideDim)
-            Instantiate(instantiateGO, new Vector2(iob.getRow()*.64f + .32f, iob.getCol()*.64f + .32f), Quaternion.identity);
+            
+            InstantiateAtPos(instantiateGO, iob.getRow(), iob.getCol());
+        }
+    }
+
+    // Places a game object at the given coordinates
+    private void InstantiateAtPos(GameObject go, int row, int col)
+    {
+        // Vector dimensions : (girdPos * scale) + (.5 * modelSideDim)
+        Vector2 placementVector = new Vector2(row * .64f + .32f, col * .64f + .32f);
+
+        Instantiate(go, placementVector, Quaternion.identity);
+    }
+
+    // Gets a random GameObject from a GameObject Array
+    private GameObject RandomFromArray(GameObject[] array)
+    {
+        return array[Random.Range(0, array.Length - 1)];
+    }
+
+    // Generates the outer wall of the board
+    void GenerateWall()
+    {
+        for( int i = -1; i <= boardDim; i++)
+        {
+            for ( int k = -1; k <= boardDim; k = boardDim)
+            {
+                InstantiateAtPos(RandomFromArray(wallPrefabs), i, k);
+                InstantiateAtPos(RandomFromArray(wallPrefabs), k, i);
+                if (k == boardDim) break;
+            }
         }
     }
 
@@ -85,8 +117,11 @@ public class GameManager : MonoBehaviour {
         // Generate initQueue
         QueueInitObstacles();
 
-        //Place objects on grid
+        // Place objects on grid
         GenerateGrid();
+
+        // Form a wall of basic blocks around the outside of the board
+        GenerateWall();
 
 	}
 }
